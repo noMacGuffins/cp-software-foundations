d7d80ba779e0c9c7848932b0107ef897657a9715
Sehan MacPro <ianparkfirst@gmail.com>
2025-02-05 20:44:34 +0900

    feat: lf original + inversion

--- volume_1/IndProp.v
+++ volume_1/IndProp.v
@@ -2,3 +2,315 @@ Set Warnings "-notation-overridden,-parsing,-deprecated-hint-without-locality".
 From LF Require Export Logic.
 
 (* Inductively Defined Propositions *)
+
+Fixpoint div2 (n : nat) : nat :=
+  match n with
+    0 => 0
+  | 1 => 0
+  | S (S n) => S (div2 n)
+  end.
+Definition csf (n : nat) : nat :=
+  if even n then div2 n
+  else (3 * n) + 1.
+
+Fail Fixpoint reaches1_in (n : nat) : nat :=
+  if n =? 1 then 0
+  else 1 + reaches1_in (csf n).
+
+Fail Fixpoint Collatz_holds_for (n : nat) : Prop :=
+  match n with
+  | 0 => False
+  | 1 => True
+  | _ => if even n then Collatz_holds_for (div2 n)
+                   else Collatz_holds_for ((3 * n) + 1)
+  end.
+
+(* inductive definition *)
+Inductive Collatz_holds_for : nat -> Prop :=
+  | Chf_one : Collatz_holds_for 1
+  | Chf_even (n : nat) : even n = true ->
+                         Collatz_holds_for (div2 n) ->
+                         Collatz_holds_for n
+  | Chf_odd (n : nat) : even n = false ->
+                         Collatz_holds_for ((3 * n) + 1) ->
+                         Collatz_holds_for n.
+
+(* for particular numbers *)
+Example Collatz_holds_for_12 : Collatz_holds_for 12.
+Proof.
+  apply Chf_even. reflexivity. simpl.
+  apply Chf_even. reflexivity. simpl.
+  apply Chf_odd. reflexivity. simpl.
+  apply Chf_even. reflexivity. simpl.
+  apply Chf_odd. reflexivity. simpl.
+  apply Chf_even. reflexivity. simpl.
+  apply Chf_even. reflexivity. simpl.
+  apply Chf_even. reflexivity. simpl.
+  apply Chf_even. reflexivity. simpl.
+  apply Chf_one.
+Qed.
+
+Conjecture collatz : forall n, n <> 0 -> Collatz_holds_for n.
+
+Inductive le : nat -> nat -> Prop :=
+  | le_n (n : nat) : le n n
+  | le_S (n m : nat) : le n m -> le n (S m).
+Notation "n <= m" := (le n m) (at level 70).
+
+(* Example: Transitive Closure *)
+
+Inductive clos_trans {X: Type} (R: X->X->Prop) : X->X->Prop :=
+  | t_step (x y : X) :
+      R x y ->
+      clos_trans R x y
+  | t_trans (x y z : X) :
+      clos_trans R x y ->
+      clos_trans R y z ->
+      clos_trans R x z.
+
+Inductive Person : Type := Sage | Cleo | Ridley | Moss.
+(* parent is not transitive *)
+Inductive parent_of : Person -> Person -> Prop :=
+  po_SC : parent_of Sage Cleo
+| po_SR : parent_of Sage Ridley
+| po_CM : parent_of Cleo Moss.
+
+(* ancestor of is transitive *)
+Definition ancestor_of : Person -> Person -> Prop :=
+  clos_trans parent_of.
+
+(** Here is a derivation showing that Sage is an ancestor of Moss:
+
+ ———————————————————(po_SC)     ———————————————————(po_CM)
+ parent_of Sage Cleo            parent_of Cleo Moss
+—————————————————————(t_step)  —————————————————————(t_step)
+ancestor_of Sage Cleo          ancestor_of Cleo Moss
+————————————————————————————————————————————————————(t_trans)
+                ancestor_of Sage Moss
+*)
+
+Example ancestor_of_ex : ancestor_of Sage Moss.
+Proof.
+  unfold ancestor_of. apply t_trans with Cleo.
+  - apply t_step. apply po_SC.
+  - apply t_step. apply po_CM. Qed.
+
+(* Reflexive and Transitive Closure *)
+
+Inductive clos_refl_trans {X: Type} (R: X->X->Prop) : X->X->Prop :=
+  | rt_step (x y : X) :
+      R x y ->
+      clos_refl_trans R x y
+  | rt_refl (x : X) :
+      clos_refl_trans R x x 
+      (* this rule added *)
+  | rt_trans (x y z : X) :
+      clos_refl_trans R x y ->
+      clos_refl_trans R y z ->
+      clos_refl_trans R x z.
+
+(* binary relation corresponding to
+    the Collatz step function *)
+Definition cs (n m : nat) : Prop := csf n = m.
+Definition cms n m := clos_refl_trans cs n m.
+Conjecture collatz' : forall n, n <> 0 -> cms n 1.
+
+
+(* Exercise: clos_refl_trans_sym *)
+
+Inductive clos_refl_trans_sym {X: Type} (R: X->X->Prop) : X->X->Prop :=
+  | rts_step (x y : X) :
+      R x y ->
+      clos_refl_trans_sym R x y
+  | rts_refl (x : X) :
+      clos_refl_trans_sym R x x 
+  | rts_trans (x y z : X) :
+      clos_refl_trans_sym R x y ->
+      clos_refl_trans_sym R y z ->
+      clos_refl_trans_sym R x z
+  | rts_sym (x y : X) :
+      clos_refl_trans_sym R x y ->
+      clos_refl_trans_sym R y x.
+
+(* Exercise end *)
+
+(* Permutations *)
+Inductive Perm3 {X : Type} : list X -> list X -> Prop :=
+  | perm3_swap12 (a b c : X) :
+      Perm3 [a;b;c] [b;a;c]
+  | perm3_swap23 (a b c : X) :
+      Perm3 [a;b;c] [a;c;b]
+  | perm3_trans (l1 l2 l3 : list X) :
+      Perm3 l1 l2 -> Perm3 l2 l3 -> Perm3 l1 l3.
+
+(* Exercise: perm *)
+(* yes *)
+(* Exercise end *)
+
+(* Evenness *)
+Inductive ev : nat -> Prop :=
+  | ev_0                       : ev 0
+  | ev_SS (n : nat) (H : ev n) : ev (S (S n)).
+
+Fail Inductive wrong_ev (n : nat) : Prop :=
+  | wrong_ev_0 : wrong_ev 0
+  (* ===> in the above line  
+  Error: Last occurrence of "[wrong_ev]" must have "[n]" as 1st
+        argument in "[wrong_ev 0]". *)
+  | wrong_ev_SS (H: wrong_ev n) : wrong_ev (S (S n)).
+
+Check ev_0 : ev 0.
+Check ev_SS : forall (n : nat), ev n -> ev (S (S n)).
+
+Module EvPlayground.
+
+Inductive ev : nat -> Prop :=
+  | ev_0  : ev 0
+  | ev_SS : forall (n : nat), ev n -> ev (S (S n)).
+
+End EvPlayground.
+
+Theorem ev_4 : ev 4.
+Proof. apply ev_SS. apply ev_SS. apply ev_0. Qed.
+
+Theorem ev_4' : ev 4.
+Proof. apply (ev_SS 2 (ev_SS 0 ev_0)). Qed.
+
+Theorem ev_plus4 : forall n, ev n -> ev (4 + n).
+Proof.
+  intros n. simpl. intros Hn.  apply ev_SS. apply ev_SS. apply Hn.
+Qed.
+
+
+(* Exercise: ev_double *)
+
+Theorem ev_double : forall n,
+  ev (double n).
+Proof.
+  intros n. induction n as [| n' IHn'].
+  - simpl. apply ev_0.
+  - simpl. apply ev_SS. apply IHn'. Qed.
+
+(* Exercise end *)
+
+(* Constructing Evidence for Permutations *)
+      
+Lemma Perm3_rev : Perm3 [1;2;3] [3;2;1].
+Proof.
+  apply perm3_trans with (l2:=[2;3;1]).
+  - apply perm3_trans with (l2:=[2;1;3]).
+    + apply perm3_swap12.
+    + apply perm3_swap23.
+  - apply perm3_swap12.
+Qed.
+
+(** **** Exercise: 1 star, standard (Perm3) *)
+Lemma Perm3_ex1 : Perm3 [1;2;3] [2;3;1].
+Proof.
+  apply perm3_trans with (l2:=[2;1;3]).
+    + apply perm3_swap12.
+    + apply perm3_swap23.
+Qed.
+
+Lemma Perm3_refl : forall (X : Type) (a b c : X),
+  Perm3 [a;b;c] [a;b;c].
+Proof.
+  intros.
+  apply perm3_trans with (l2:=[a;c;b]).
+  - apply perm3_swap23.
+  - apply perm3_swap23.
+Qed.
+(* Exercise end *)
+
+
+(** ** Destructing and Inverting Evidence *)
+Theorem ev_inversion : forall (n : nat),
+    ev n ->
+    (n = 0) \/ (exists n', n = S (S n') /\ ev n').
+Proof.
+  intros n E.  destruct E as [ | n' E'] eqn:EE.
+  - (* E = ev_0 : ev 0 *)
+    left. reflexivity.
+  - (* E = ev_SS n' E' : ev (S (S n')) *)
+    right. exists n'. split. reflexivity. apply E'.
+Qed.
+
+
+(** **** Exercise: 1 star, standard (le_inversion)
+
+    Let's prove a similar inversion lemma for [le]. *)
+Theorem le_inversion : forall (n m : nat),
+  le n m ->
+  (n = m) \/ (exists m', m = S m' /\ le n m').
+Proof.
+  intros n m H. destruct H as [ | m' H'] eqn:HH.
+  - left. reflexivity.
+  - right. exists H'. split.
+    + reflexivity.
+    + apply l.
+Qed.
+
+(* Exercise end *)
+
+
+Theorem evSS_ev : forall n, ev (S (S n)) -> ev n.
+Proof.
+  intros n E. apply ev_inversion in E. destruct E as [H0|H1].
+  - discriminate H0.
+  - destruct H1 as [n' [Hnn' E']]. injection Hnn' as Hnn'.
+    rewrite Hnn'. apply E'.
+Qed.
+
+Theorem evSS_ev' : forall n,
+  ev (S (S n)) -> ev n.
+Proof.
+  intros n E.  inversion E as [| n' E' Hnn'].
+  (* We are in the [E = ev_SS n' E'] case now. *)
+  apply E'.
+Qed.
+
+Theorem one_not_even : ~ ev 1.
+  intros H. apply ev_inversion in H.  destruct H as [ | [m [Hm _]]].
+Proof.
+  - discriminate H.
+  - discriminate Hm.
+Qed.
+
+Theorem one_not_even' : ~ ev 1.
+Proof. intros H. inversion H. Qed.
+
+(** **** Exercise: 1 star, standard (inversion_practice) *)
+
+Theorem SSSSev__even : forall n,
+  ev (S (S (S (S n)))) -> ev n.
+Proof.
+  intros n H. inversion H as [| n' H' Hn]. inversion H'. apply H1. 
+Qed.
+
+(* Exercise end *)
+
+(** **** Exercise: 1 star, standard (ev5_nonsense)
+
+    Prove the following result using [inversion]. *)
+
+Theorem ev5_nonsense :
+  ev 5 -> 2 + 2 = 9.
+Proof.
+  intros H. inversion H as [| n' H' Hn]. inversion H' as [| n'' H'' Hn']. inversion H''. Qed.
+(* Exercise end *)
+
+(** The [inversion] tactic does quite a bit of work. For
+    example, when applied to an equality assumption, it does the work
+    of both [discriminate] and [injection]. In addition, it carries
+    out the [intros] and [rewrite]s that are typically necessary in
+    the case of [injection]. *)
+
+
+
+
+
+
+
+
+
+
