5b561cf644ba0737c406aefc1d3c1337bc848878
Sehan MacPro <ianparkfirst@gmail.com>
2025-02-15 00:37:12 +0900

    feat: plus_le_facts2

--- volume_1/IndProp.v
+++ volume_1/IndProp.v
@@ -305,6 +305,347 @@ Proof.
     out the [intros] and [rewrite]s that are typically necessary in
     the case of [injection]. *)
 
+Theorem inversion_ex1 : forall (n m o : nat),
+  [n; m] = [o; o] -> [n] = [m].
+Proof.
+  intros n m o H. inversion H. reflexivity. Qed.
+
+Theorem inversion_ex2 : forall (n : nat),
+  S n = O -> 2 + 2 = 5.
+Proof.
+  intros n contra. inversion contra. Qed.
+
+(** Here's how [inversion] works in general.
+      - Suppose inductive proposition P and assumption H for P.
+      - For each of the constructors of [P], inversion (1: generates a subgoal) with
+        H replaced by the arguments of that constructor.
+      - (2: Discrimate) on self-contradictory subgoals
+      - (3: Intros) for the left over goals *)
+
+(* proof of coinciding definitions of evenness with inversion *)
+Lemma ev_Even_firsttry : forall n,
+  ev n -> Even n.
+Proof.
+  (* WORKED IN CLASS *) unfold Even.
+  intros n E. inversion E as [EQ' | n' E' EQ'].
+  - (* E = ev_0 *) exists 0. reflexivity.
+  - assert (H: (exists k', n' = double k')
+               -> (exists n0, S (S n') = double n0)).
+        { intros [k' EQ'']. exists (S k'). simpl.
+          rewrite <- EQ''. reflexivity. }
+    apply H.
+    generalize dependent E'.
+  Abort.
+
+(** ** Induction on Evidence *)
+Lemma ev_Even : forall n,
+  ev n -> Even n.
+Proof.
+  unfold Even. intros n E.
+  induction E as [|n' E' IH].
+  - (* E = ev_0 *)
+    exists 0. reflexivity.
+  - (* E = ev_SS n' E',  with IH : Even n' *)
+    destruct IH as [k Hk]. rewrite Hk.
+    exists (S k). simpl. reflexivity.
+Qed.
+
+(** The equivalence between the second and third definitions of
+    evenness now follows. *)
+
+Theorem ev_Even_iff : forall n,
+  ev n <-> Even n.
+Proof.
+  intros n. split.
+  - (* -> *) apply ev_Even.
+  - (* <- *) unfold Even. intros [k Hk]. rewrite Hk. apply ev_double.
+Qed.
+
+
+(** **** Exercise: 2 stars, standard (ev_sum) *)
+Theorem ev_sum : forall n m, ev n -> ev m -> ev (n + m).
+Proof.
+  intros n m Hn Hm. induction Hn as [| n' Hn' IH].
+  - simpl. apply Hm.
+  - simpl. apply ev_SS. apply IH.
+Qed.
+(* Exercise end *)
+
+(** **** Exercise: 3 stars, advanced, especially useful (ev_ev__ev) *)
+Theorem ev_ev__ev : forall n m,
+  ev (n+m) -> ev n -> ev m.
+  (* Hint: There are two pieces of evidence you could attempt to induct upon
+      here. If one doesn't work, try the other. *)
+Proof.
+  intros n m Hnm Hn. induction Hn as [| n' Hn' IH].
+  - simpl in Hnm. apply Hnm.
+  - simpl in Hnm. inversion Hnm as [| n'' Hnm' Hnm''].
+    apply IH. apply Hnm'.
+Qed.
+(* Exercise end *)
+
+(** **** Exercise: 3 stars, standard, especially useful (ev_plus_plus) *)
+
+Theorem ev_plus_plus : forall n m p,
+  ev (n+m) -> ev (n+p) -> ev (m+p).
+Proof.
+  intros n m p Hnm Hnp. 
+  assert (H: ev ((n + m) + (n + p))).
+  { apply ev_sum.
+    - apply Hnm. 
+    - apply Hnp. }
+  rewrite add_comm with n m in H.
+  rewrite <- add_assoc with m n (n + p) in H.
+  rewrite add_assoc with n n p in H.
+  rewrite add_comm with (n + n) p in H.
+  rewrite add_assoc with m p (n + n) in H.
+  rewrite add_comm with (m + p) (n + n) in H.
+  apply ev_ev__ev with (n + n).
+  - apply H.
+  - rewrite <- double_plus. apply ev_double.
+Qed.
+(* Exercise end *)
+
+(** **** Exercise: 4 stars, advanced, optional (ev'_ev)
+
+    In general, there may be multiple ways of defining a
+    property inductively.  For example, here's a (slightly contrived)
+    alternative definition for [ev]: *)
+
+Inductive ev' : nat -> Prop :=
+  | ev'_0 : ev' 0
+  | ev'_2 : ev' 2
+  | ev'_sum n m (Hn : ev' n) (Hm : ev' m) : ev' (n + m).
+
+
+Theorem ev'_ev : forall n, ev' n <-> ev n.
+Proof.
+  intros n. split.
+  - intros H. induction H.
+    + apply ev_0.
+    + apply ev_SS. apply ev_0.
+    + apply ev_sum.
+      * apply IHev'1.
+      * apply IHev'2.
+  - intros H. induction H.
+    + apply ev'_0.
+    + assert (H' : ev' (S (S n)) = ev' (2 + n)). {
+      rewrite <- plus_1_l. rewrite <- plus_n_Sm. rewrite <- plus_1_l. simpl.
+      reflexivity.
+    }
+      rewrite -> H'. apply ev'_sum.
+      * apply ev'_2.
+      * apply IHev.
+Qed.
+(* Exercise end *)
+
+(** We can do similar inductive proofs on the [Perm3] relation,
+    which we defined earlier as follows: *)
+
+Module Perm3Reminder.
+
+Inductive Perm3 {X : Type} : list X -> list X -> Prop :=
+  | perm3_swap12 (a b c : X) :
+      Perm3 [a;b;c] [b;a;c]
+  | perm3_swap23 (a b c : X) :
+      Perm3 [a;b;c] [a;c;b]
+  | perm3_trans (l1 l2 l3 : list X) :
+      Perm3 l1 l2 -> Perm3 l2 l3 -> Perm3 l1 l3.
+
+End Perm3Reminder.
+
+Lemma Perm3_symm : forall (X : Type) (l1 l2 : list X),
+  Perm3 l1 l2 -> Perm3 l2 l1.
+Proof.
+  intros X l1 l2 E.
+  induction E as [a b c | a b c | l1 l2 l3 E12 IH12 E23 IH23].
+  - apply perm3_swap12.
+  - apply perm3_swap23.
+  - apply (perm3_trans _ l2 _).
+    * apply IH23.
+    * apply IH12.
+Qed.
+
+(** **** Exercise: 2 stars, standard (Perm3_In) *)
+Lemma Perm3_In : forall (X : Type) (x : X) (l1 l2 : list X),
+    Perm3 l1 l2 -> In x l1 -> In x l2.
+Proof.
+  intros X x l1 l2 H1 H2. induction H1 as [a b c | a b c | l1 l2 l3 H1 IH1' H2' IH2'].
+  - simpl in H2. destruct H2 as [H2 | [H2 | H2]].
+    + rewrite H2. right. left. reflexivity.
+    + rewrite H2. left. reflexivity.
+    + right. right. apply H2.
+  - simpl in H2. destruct H2 as [H2 | [H2 | [H2 | H2]]].
+    + rewrite H2. left. reflexivity.
+    + rewrite H2. right. right. left. reflexivity.
+    + right. left. apply H2.
+    + right. right. right. apply H2. 
+  - apply IH2'. apply IH1'. apply H2.
+Qed.
+(* Exercise end *)
+
+(** **** Exercise: 1 star, standard, optional (Perm3_NotIn) *)
+Lemma Perm3_NotIn : forall (X : Type) (x : X) (l1 l2 : list X),
+    Perm3 l1 l2 -> ~In x l1 -> ~In x l2.
+Proof.
+  intros X x l1 l2 H1 H2 H3. apply H2. apply Perm3_In with (l1:=l2).
+  - apply Perm3_symm. apply H1.
+  - apply H3.
+Qed.
+(* Exercise end *)
+
+(** **** Exercise: 2 stars, standard, optional (NotPerm3)
+
+    Proving that something is NOT a permutation is quite tricky. Some
+    of the lemmas above, like [Perm3_In] can be useful for this. *)
+Example Perm3_example2 : ~ Perm3 [1;2;3] [1;2;4].
+Proof.
+  intros H. apply Perm3_In with (x:=3) in H.
+  - simpl in H. destruct H as [H | [H | [H | H]]].
+    + discriminate H.
+    + discriminate H.
+    + discriminate H.
+    + apply H.
+  - simpl. right. right. left. reflexivity.
+Qed.
+
+(** * Exercising with Inductive Relations *)
+
+(** A proposition parameterized by a number (such as [ev])
+    can be thought of as a _property_ -- i.e., it defines
+    a subset of [nat], namely those numbers for which the proposition
+    is provable.  In the same way, a two-argument proposition can be
+    thought of as a _relation_ -- i.e., it defines a set of pairs for
+    which the proposition is provable. *)
+
+Module Playground.
+
+(** Just like properties, relations can be defined inductively.  One
+    useful example is the "less than or equal to" relation on numbers
+    that we briefly saw above. *)
+
+Inductive le : nat -> nat -> Prop :=
+  | le_n (n : nat)                : le n n
+  | le_S (n m : nat) (H : le n m) : le n (S m).
+
+Notation "n <= m" := (le n m).
+
+Theorem test_le1 :
+  3 <= 3.
+Proof.
+  (* WORKED IN CLASS *)
+  apply le_n.  Qed.
+
+Theorem test_le2 :
+  3 <= 6.
+Proof.
+  (* WORKED IN CLASS *)
+  apply le_S. apply le_S. apply le_S. apply le_n.  Qed.
+
+Theorem test_le3 :
+  (2 <= 1) -> 2 + 2 = 5.
+Proof.
+  (* WORKED IN CLASS *)
+  intros H. inversion H. inversion H2.  Qed.
+
+(** The "strictly less than" relation [n < m] can now be defined
+    in terms of [le]. *)
+
+Definition lt (n m : nat) := le (S n) m.
+
+Notation "n < m" := (lt n m).
+
+(** The [>=] operation is defined in terms of [<=]. *)
+
+Definition ge (m n : nat) : Prop := le n m.
+Notation "m >= n" := (ge m n).
+
+End Playground.
+
+(** **** Exercise: 3 stars, standard, especially useful (le_facts) *)
+Lemma le_trans : forall m n o, m <= n -> n <= o -> m <= o.
+Proof.
+  intros m n o H1 H2. induction H2 as [| n' o H2' H2'' IH].
+  - apply H1.
+  - apply le_S. apply H2''. apply H1.
+Qed.
+
+Theorem O_le_n : forall n,
+  0 <= n.
+Proof.
+  intros n. induction n as [| n' IH].
+  - apply le_n.
+  - apply le_S. apply IH.
+Qed.
+
+Theorem n_le_m__Sn_le_Sm : forall n m,
+  n <= m -> S n <= S m.
+Proof.
+  intros n m H. induction H as [| n' m' H' IH].
+  - apply le_n.
+  - apply le_S. apply IH.
+Qed.
+
+Theorem Sn_le_Sm__n_le_m : forall n m,
+  S n <= S m -> n <= m.
+Proof.
+  intros n m H. induction m.
+  - inversion H. 
+    + apply le_n.
+    + inversion H2.  
+  - inversion H.
+    + apply le_n.
+    + apply le_S. apply IHm. apply H2.
+Qed.
+
+Theorem le_plus_l : forall a b,
+  a <= a + b.
+Proof.
+  intros a b. induction b.
+  - rewrite add_0_r. apply le_n.
+  - rewrite add_comm with a (S b). simpl. apply le_S. rewrite add_comm. apply IHb.
+Qed.
+
+(* Exercise end *)
+
+(** **** Exercise: 2 stars, standard, especially useful (plus_le_facts1) *)
+
+Theorem plus_le : forall n1 n2 m,
+  n1 + n2 <= m ->
+  n1 <= m /\ n2 <= m.
+Proof.
+  intros n1 n2 m H. split.
+  - induction n2.
+    + rewrite add_0_r in H. apply H.
+    + rewrite add_comm with n1 (S n2) in H. apply le_S in H. simpl in H. 
+    apply Sn_le_Sm__n_le_m in H. rewrite add_comm in H. apply IHn2. apply H. 
+  - induction n1.
+    + simpl in H. apply H.
+    + simpl in H. apply le_S in H. rewrite add_comm in H. 
+    apply Sn_le_Sm__n_le_m in H. rewrite add_comm in H. apply IHn1. apply H. 
+Qed. 
+
+Theorem plus_le_cases : forall n m p q,
+  n + m <= p + q -> n <= p \/ m <= q.
+  (** Hint: May be easiest to prove by induction on [n]. *)
+Proof.
+  induction n.
+  - left. apply O_le_n.
+  - destruct p.
+    + right. apply plus_le in H.
+      destruct H as [H1 H2].
+      rewrite plus_O_n in H1.
+      apply H2.
+    + intros.  simpl in H. apply Sn_le_Sm__n_le_m in H. apply IHn in H. destruct H.
+      * left. apply n_le_m__Sn_le_Sm. apply H. 
+      * right. apply H.
+Qed.
+
+(* Exercise end *)
+
+(** **** Exercise: 2 stars, standard, especially useful (plus_le_facts2) *)
+
+
 
 
 
